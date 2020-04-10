1b9a856e0f2ecc418dd0ce00926ada52adde5a30
John Studarus <john@jhlconsulting.com>
2020-04-09 18:46:07 -0700

    Train update

--- ComputeNova.sh
+++ ComputeNova.sh
@@ -9,27 +9,27 @@ MY_IP=`hostname -I | xargs -n1 | grep "^10\." | head -1`
 # nova
 apt-get -y install nova-compute
 
-apt-get -y install nova-serialproxy nova-console
+apt-get -y install nova-serialproxy
 
-crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:RABBIT_PASS@${CONTROLLER_PRIVATE_IP}
+crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:RABBIT_PASS@controller
 
 crudini --set /etc/nova/nova.conf api auth_strategy keystone
 
-crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://controller:5000
-crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:35357
+crudini --set /etc/nova/nova.conf keystone_authtoken www_authenticate_uri http://controller:5000/
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:5000/
 crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers controller:11211
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_type password
-crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name default
-crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name default
+crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name Default
 crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
 crudini --set /etc/nova/nova.conf keystone_authtoken username nova
 crudini --set /etc/nova/nova.conf keystone_authtoken password NOVA_PASS
 
 crudini --set /etc/nova/nova.conf DEFAULT my_ip ${MY_IP}
-crudini --set /etc/nova/nova.conf DEFAULT use_neutron True
+crudini --set /etc/nova/nova.conf DEFAULT use_neutron true
 crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
 
-crudini --set /etc/nova/nova.conf vnc enabled True
+crudini --set /etc/nova/nova.conf vnc enabled true
 crudini --set /etc/nova/nova.conf vnc vncserver_listen 0.0.0.0
 crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address ${MY_IP}
 crudini --set /etc/nova/nova.conf vnc novncproxy_base_url http://${CONTROLLER_PUBLIC_IP}:6080/vnc_auto.html
@@ -38,15 +38,12 @@ crudini --set /etc/nova/nova.conf glance api_servers http://controller:9292
 
 crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
 
-# Due to a packaging bug, remove the log_dir option from the [DEFAULT] section.
-crudini --del /etc/nova/nova.conf DEFAULT log_dir
-
 crudini --set /etc/nova/nova.conf placement os_region_name RegionOne
 crudini --set /etc/nova/nova.conf placement project_domain_name Default
 crudini --set /etc/nova/nova.conf placement project_name service
 crudini --set /etc/nova/nova.conf placement auth_type password
 crudini --set /etc/nova/nova.conf placement user_domain_name Default
-crudini --set /etc/nova/nova.conf placement auth_url http://controller:35357/v3
+crudini --set /etc/nova/nova.conf placement auth_url http://controller:5000/v3
 crudini --set /etc/nova/nova.conf placement username placement
 crudini --set /etc/nova/nova.conf placement password PLACEMENT_PASS
 
