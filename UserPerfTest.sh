96a7f73d793d9004e2e1663ea750e6ce93672e6f
John Studarus <john@jhlconsulting.com>
2020-04-10 08:49:26 -0700

    Train update

--- UserPerfTest.sh
+++ UserPerfTest.sh
@@ -1,11 +1,11 @@
-#/bin/bash
+5000bin/bash
 
 export OS_USERNAME=admin
 export OS_PASSWORD=ADMIN_PASS
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_DOMAIN_NAME=Default
-export OS_AUTH_URL=http://controller:35357/v3
+export OS_AUTH_URL=http://controller:5000/v3
 export OS_IDENTITY_API_VERSION=3
 
 errors=0
