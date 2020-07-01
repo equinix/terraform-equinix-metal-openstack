6f6f4f30b11687622a22939f31c757b6c4e49045
johnstudarus <john@jhlconsulting.com>
2020-07-01 16:57:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "packet-openstack"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
