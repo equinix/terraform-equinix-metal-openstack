484e356f7a364625064df875bf7a51f32697c795
johnstudarus <john@jhlconsulting.com>
2020-07-02 15:15:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # The configuration for the `remote` backend.
-#terraform {
-#  backend "remote" {
-#    # The name of your Terraform Cloud organization.
-#    organization = "packet-openstack"
-#
-#    # The name of the Terraform Cloud workspace to store Terraform state files in.
-#    workspaces {
-#      name = "prod"
-#    }
-#  }
-#}
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "packet-openstack"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "prod"
+    }
+  }
+}
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
