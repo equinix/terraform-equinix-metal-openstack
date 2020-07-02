fe0992b03061924fdc7089443d5dcc97889ab217
johnstudarus <john@jhlconsulting.com>
2020-07-02 15:15:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,3 @@ terraform {
     }
   }
 }
-
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
