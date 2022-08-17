b0cd313d0304b78711a83aafab37285c3770ffe6
Marques Johansson <mjohansson@equinix.com>
2022-08-17 14:18:58 -0400

    change provider from deprecated Metal provider to Equinix
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- ProviderNetwork.tf
+++ ProviderNetwork.tf
@@ -4,7 +4,7 @@
 
 # use private IPv4 addresses to save precious Internet resources
 data "equinix_metal_precreated_ip_block" "private_ipv4" {
-
+  metro          = equinix_metal_device.controller.metro
   project_id     = local.metal_project_id
   address_family = 4
   public         = false
@@ -13,7 +13,6 @@ data "equinix_metal_precreated_ip_block" "private_ipv4" {
 
 # /28 provides 16 IPs
 resource "equinix_metal_ip_attachment" "controller_private_ipv4" {
-
   device_id     = equinix_metal_device.controller.id
   cidr_notation = cidrsubnet(data.equinix_metal_precreated_ip_block.private_ipv4.cidr_notation, 3, 1)
 
@@ -21,7 +20,7 @@ resource "equinix_metal_ip_attachment" "controller_private_ipv4" {
 
 # ipv6 is free so let's go crazy
 data "equinix_metal_precreated_ip_block" "public_ipv6" {
-
+  metro          = equinix_metal_device.controller.metro
   project_id     = local.metal_project_id
   address_family = 6
   public         = true
@@ -29,7 +28,6 @@ data "equinix_metal_precreated_ip_block" "public_ipv6" {
 }
 
 resource "equinix_metal_ip_attachment" "controller_public_ipv6" {
-
   device_id     = equinix_metal_device.controller.id
   cidr_notation = cidrsubnet(data.equinix_metal_precreated_ip_block.public_ipv6.cidr_notation, 8, 1)
 
